b9c5e25e980fe9979dfd1b8b2076e00c721c5eb1
Lusanele <162572862+KumkaniGoqoza@users.noreply.github.com>
2025-11-07 16:33:00 +0200

    Update Main.java

--- src/main/java/numbersummarizer/Main.java
+++ src/main/java/numbersummarizer/Main.java
@@ -1,7 +1,10 @@
 package main.java.numbersummarizer;
 
 import java.util.Collection;
-//This file is for my own manual testing of the methods. To test if they work and if they produce the required results
+//This file is my own manual testing of the methods. To test if they work and if they produce the required results
+//I have no extensive experience with Java so I am using this file as a form of unit testing
+//Writing up Maven based test units proved to be challenging and I wanted to implement it with my own knowledge
+//Intead of AI, which at the moment is not sufficient enough. The rources I consulted would need me to spend more than just 7 days
 public class Main {
 
     public static void main(String[] args) {
@@ -18,6 +21,7 @@ public class Main {
         // 4. Calling the summarizeCollection method and print the result
         String summary = summarizer.summarizeCollection(numbers);
         System.out.println("Result: " + summary);
+        System.out.println("Expected: 1, 3, 6-8, 12-15, 21-24, 31");
         
     }
 }
